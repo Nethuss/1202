bc68f21fddac285a8e25152e38149612426e7489
Nethuss <ddidcvee@gmail.com>
2025-12-03 03:04:00 +0300

    [#100] Make chnages

--- obj/Debug/net8.0/Success.AssemblyInfo.cs
+++ obj/Debug/net8.0/Success.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Success")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996c7cb0984593dba307fce0356a8f5276dadae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Success")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Success")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
